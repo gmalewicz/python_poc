9dddd5528c8782a086bc699f5ae675116bf1a4be
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2025-06-02 13:48:41 +0200

    initial version

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'host': socket.gethostname(),
-        'message': "Welcome 8!"
+        'message': "Welcome 9!"
     })
 
 @app.route('/api/v1/health')
